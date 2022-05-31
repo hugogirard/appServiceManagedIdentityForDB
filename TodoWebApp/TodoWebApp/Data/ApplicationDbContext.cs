10dc2bcbc6e4ffc046eb41574c4ac6ad75897759
Sen Wang <sen.wang@mail.mcgill.ca>
2022-05-31 09:33:58 -0400

    create mvc template and todo model

---
+++ TodoWebApp/TodoWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TodoWebApp.Models;
+
+namespace TodoWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<TodoWebApp.Models.Todo>? Todo { get; set; }
+    }
+}
---
+++ TodoWebApp/TodoWebApp/Models/Todo.cs
@@ -0,0 +1,16 @@
+﻿namespace TodoWebApp.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public Todo()
+        {
+
+        }
+    }
+}
